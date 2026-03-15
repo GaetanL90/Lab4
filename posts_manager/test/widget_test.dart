c1fd6ea8a1cee0ca76b18d1c13e97909a76a7aa4
GaetanL90 <gyitgaetan90@gmail.com>
2026-03-15 19:30:00 +0200

    update -2

--- posts_manager/test/widget_test.dart
+++ posts_manager/test/widget_test.dart
@@ -7,9 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:lab4_app/main.dart';
-
+import 'package:posts_manager/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
